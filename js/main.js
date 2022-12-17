3d92fe13e65e2206f7a26b54637b11408705a1a3
safroigor <safroigor@yandex.ru>
2022-12-17 14:34:33 +0300

    finish responsive design

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav');
+const body = document.body;
 
-navBtn.addEventListener('click', function() {
+navBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    toggleMobileNav();
+})
+
+window.addEventListener('click', function() {
+
+    if (body.classList.contains('no-scroll')) {
+        toggleMobileNav();           
+    }
+})
+
+mobileNav.addEventListener('click', function(event) {
+    event.stopPropagation();
+})
+
+function toggleMobileNav() {
     mobileNav.classList.toggle('mobile-nav-active');
     navBtn.classList.toggle('nav-button-close');
-})
+    body.classList.toggle('no-scroll');
+}
